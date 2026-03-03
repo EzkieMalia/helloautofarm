a649f5f0ecec40ced186bb7e8996389b5525f215
EzkieMalia <ezekielquisky1z@outlook.com>
2026-03-02 17:02:35 -0700

    Update Autofarm.lua

--- Autofarm.lua
+++ Autofarm.lua
@@ -1,3 +1,4 @@
 queue_on_teleport([[
+    repeat task.wait() until game:IsLoaded()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzkieMalia/helloautofarm/refs/heads/main/RealAutofarm.lua"))()
 ]])
