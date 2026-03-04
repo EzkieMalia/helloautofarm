1cc3c5b38d7bd23ec44c4257f33aa31b878626f5
EzkieMalia <ezekielquisky1z@outlook.com>
2026-03-04 15:06:01 -0700

    Update Autofarm.lua

--- Autofarm.lua
+++ Autofarm.lua
@@ -2,7 +2,7 @@ task.spawn(function()
 queue_on_teleport([[
     repeat task.wait() until game:IsLoaded()
     if game.PlaceId ~= 15124180230 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/EzkieMalia/helloautofarm/refs/heads/main/Autoexecute.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EzkieMalia/Autofarm/refs/heads/main/Autoexecute.lua"))()
     else
         print("Successfully rejoined.")
         loadstring(game:HttpGet("https://raw.githubusercontent.com/EzkieMalia/helloautofarm/refs/heads/main/RealAutofarm.lua"))()
